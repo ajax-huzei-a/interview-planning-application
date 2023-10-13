e59cece4b48ae11a1b2a4d05e2076dbf402ab806
arturhuzei <huzei.a@ajax.systems>
2023-10-13 19:31:24 +0300

    Refactor Period data class

--- src/main/java/intellistart/interviewplanning/model/period/Period.kt
+++ src/main/java/intellistart/interviewplanning/model/period/Period.kt
@@ -9,32 +9,12 @@ import java.time.LocalTime
 data class Period(
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    var date: LocalDate = LocalDate.now(),
+    val date: LocalDate = LocalDate.now(),
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
-    var from: LocalTime = LocalTime.now(),
+    val from: LocalTime = LocalTime.now(),
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
-    var to: LocalTime = LocalTime.now()
+    val to: LocalTime = LocalTime.now()
 
-) {
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Period
-
-        if (from != other.from) return false
-        if (to != other.to) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = date.hashCode()
-        result = 31 * result + to.hashCode()
-        result = 31 * result + from.hashCode()
-        return result
-    }
-}
+)
